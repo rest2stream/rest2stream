353bee2d0698301ecfcfcc6a46bd0087b0f6fdff
josnin <nino.capati@gmail.com>
2020-11-02 22:32:07 +0800

    - Created MyApis Models - Created Endpoint for MyApis

--- fastapi/routers/myapi.py
+++ fastapi/routers/myapi.py
@@ -1,36 +1,41 @@
-from typing import Optional
+from typing import Optional, List
 from fastapi import APIRouter
 from pydantic import BaseModel, HttpUrl
+from tortoise.contrib.fastapi import HTTPNotFoundError
 from models import MyApi_Pydantic, MyApisIn_Pydantic, MyApis
 
 router = APIRouter()
 
-class MyApi(BaseModel):
-    name: str
-    description: Optional[str] = None
-    polling_frequency: int
-    url: HttpUrl
-    http_headers: Optional[str] = None
-    query_params: Optional[str] = None
+
+class Status(BaseModel):
+    message: str
 
 
-@router.post('/items')
-async def create_item(myapi: MyApi):
-    return {'results': myapi}
+@router.get("/list-myapi", response_model=List[MyApi_Pydantic])
+async def list_myapi():
+    return await MyApi_Pydantic.from_queryset(MyApis.all())
 
+@router.post("/create-myapi", response_model=MyApi_Pydantic)
+async def create_myapi(myapi: MyApisIn_Pydantic):
+    myapi_obj = await MyApis.create(**myapi.dict(exclude_unset=True))
+    return await MyApi_Pydantic.from_tortoise_orm(myapi_obj)
 
-@router.get('/get-api/{user_id}')
-async def get_api(user_id: int, nice: Optional[int] = None):
-    return {'results': 'list of apis' }
+@router.get(
+    "/myapi/{myapi_id}", response_model=MyApi_Pydantic, responses={404: {"model": HTTPNotFoundError}}
+)
+async def get_myapi(myapi_id: int):
+    return await MyApi_Pydantic.from_queryset_single(MyApis.get(id=myapi_id))
 
-@router.post('/new-api')
-async def new_api(api: MyApi):
-    return {'results': 'created'}
+@router.put(
+    "/myapi/{myapi_id}", response_model=MyApi_Pydantic, responses={404: {"model": HTTPNotFoundError}}
+)
+async def update_myapi(myapi_id: int, myapi: MyApisIn_Pydantic):
+    await MyApis.filter(id=myapi_id).update(**myapi.dict(exclude_unset=True))
+    return await MyApi_Pydantic.from_queryset_single(MyApis.get(id=myapi_id))
 
-@router.post('/edit-api')
-async def edit_api():
-    return {'results': 'edited'}
-
-@router.post('/delete-api')
-async def delete_api():
-    return {'results': 'deleted'}
+@router.delete("/myapi/{myapi_id}", response_model=Status, responses={404: {"model": HTTPNotFoundError}})
+async def delete_myapi(myapi_id: int):
+    deleted_count = await MyApis.filter(id=myapi_id).delete()
+    if not deleted_count:
+        raise HTTPException(status_code=404, detail=f"MyApi {myapi_id} not found")
+    return Status(message=f"Deleted myapi {myapi_id}")
