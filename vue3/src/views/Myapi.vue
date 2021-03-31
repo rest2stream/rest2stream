43dc79c15bce5992202426acc065b4848325681e
josnin <nino.capati@gmail.com>
2021-03-31 20:11:20 +0800

    Designing MyApi listings

--- vue3/src/views/Myapi.vue
+++ vue3/src/views/Myapi.vue
@@ -1,7 +1,14 @@
 <template>
 
-  <div v-for="api in listOfApi">
-    {{api.url}} - {{api.frequency}}
+  <div class="myapi-grid mdl-typography--h6">
+    <div class="myapi-grid__name">Name</div>
+    <div class="myapi-grid__url">Url</div>
+    <div class="myapi-grid__frequency">Polling Frequency</div>
+    <template v-for="api in listOfApi">
+      <div class="myapi-grid__name"><a href=""><strong>{{api.name}}</strong></a></div>
+      <div class="myapi-grid__url">{{api.url}}</div>
+      <div class="myapi-grid__frequency">{{api.polling_frequency}} {{api.polling_unit}}</div>
+    </template>
   </div>
 
 </template>
@@ -14,8 +21,8 @@
     name: "MyApi",
     setup() {
       const listOfApi = reactive([
-        { url: "https://github.com/tortoise/tortoise-orm", frequency: 5},
-        { url: "https://github.com/tiangolo/fastapi/", frequency: 5}
+        { name: "NBA Sport Api", url: "https://api-sports/api/api-nba", polling_frequency: 5, polling_unit: "min."},
+        { name: "API Football", url: "https://rapidapi.com/api-sports/api/api-football", polling_frequency: 5, polling_unit: "secs."}
       ])
 
       const site = useSite();
@@ -32,3 +39,30 @@
   }
 
 </script>
+
+<style lang="scss">
+
+  .myapi-grid {
+    display: grid;
+    padding: 1rem;
+    grid-template-columns: 150px auto 100px;
+    align-items: center;
+
+    &__name {
+      padding: 1rem;
+    }
+
+    &__url {
+      padding: 1rem;
+    }
+    
+    &__frequency {
+      padding: 1rem;
+      width:100%;
+      display:grid;
+      justify-content: center;
+    }
+  }
+
+
+</style>
