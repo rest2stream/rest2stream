eb7c087dde59cca3352615dbc85879ef897cd3cd
josnin <nino.capati@gmail.com>
2021-04-04 17:44:40 +0800

    temporary allowed cross origin

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,9 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from tortoise.contrib.fastapi import register_tortoise
 
 from routers import myapi
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "http://192.168.1.88:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(myapi.router)
 
