e5091ff55524991f7d318c95556072427fca6b82
josnin <nino.capati@gmail.com>
2021-04-06 22:49:56 +0800

    change traditional function call to es6 shorthand function call

--- vue3/src/use/useMyApi.js
+++ vue3/src/use/useMyApi.js
@@ -4,14 +4,8 @@ import types from '../store/types'
 export default function useMyApi() {
 
   const store = useStore()
-
-  function create(obj) {
-    store.dispatch(`myapi/${types.CREATE_MYAPI}`, obj)
-  }
-
-  function fetch(obj) {
-    store.dispatch(`myapi/${types.FETCH_MYAPI}`)
-  }
+  const create = (obj) => store.dispatch(`myapi/${types.CREATE_MYAPI}`, obj);
+  const fetch = (obj) => store.dispatch(`myapi/${types.FETCH_MYAPI}`);
 
   return {
     create,
