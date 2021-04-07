1e3980ab294244fe6d7ba671219d8362ae457329
josnin <nino.capati@gmail.com>
2021-04-07 21:35:23 +0800

    missed out to remove obj

--- vue3/src/use/useMyApi.js
+++ vue3/src/use/useMyApi.js
@@ -5,7 +5,7 @@ export default function useMyApi() {
 
   const store = useStore()
   const create = (obj) => store.dispatch(`myapi/${types.CREATE_MYAPI}`, obj);
-  const fetch = (obj) => store.dispatch(`myapi/${types.FETCH_MYAPI}`);
+  const fetch = () => store.dispatch(`myapi/${types.FETCH_MYAPI}`);
 
   return {
     create,
